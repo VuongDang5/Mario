63c01c7ac46ab0f981a8b91c1c87e43809824085
Michael <Michael.dang@transwap.com>
2023-10-03 21:51:01 +0700

    fix game loop

--- Mario/main.cpp
+++ Mario/main.cpp
@@ -19,39 +19,17 @@
 #include <vector>
 
 #include "debug.h"
-#include "Game.h"
-#include "GameObject.h"
+#include "GameLoop.h"
 
 #define WINDOW_CLASS_NAME L"Game Window"
-#define MAIN_WINDOW_TITLE L"01 - Skeleton"
+#define MAIN_WINDOW_TITLE L"Mario"
 #define WINDOW_ICON_PATH L"C:\\Code\\Mario\\Mario\\Resource\\brick.ico"
 
-#define TEXTURE_PATH_BRICK L"C:\\Code\\Mario\\Mario\\Resource\\brick.png"
-#define TEXTURE_PATH_MARIO L"C:\\Code\\Mario\\Mario\\Resource\\mario.png"
-
-#define TEXTURE_PATH_MISC L"C:\\Code\\Mario\\Mario\\Resource\\misc.png"
-
-#define BACKGROUND_COLOR D3DXCOLOR(0.5f, 0.5f, 0.5f, 0.0f)
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 240
 
 
 using namespace std;
-
-CMario* mario;
-#define MARIO_START_X 10.0f
-#define MARIO_START_Y 100.0f
-#define MARIO_START_VX 0.1f
-#define MARIO_START_VY 0.1f
-
-
-CBrick* brick;
-#define BRICK_X 10.0f
-#define BRICK_Y 120.0f
-
-LPTEXTURE texMario = NULL;
-LPTEXTURE texBrick = NULL;
-LPTEXTURE texMisc = NULL;
 
 //vector<LPGAMEOBJECT> objects;  
 
@@ -66,87 +44,6 @@ LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	}
 
 	return 0;
-}
-
-/*
-	Load all game resources. In this example, create a brick object and mario object
-*/
-void LoadResources()
-{
-	CGame* game = CGame::GetInstance();
-	texBrick = game->LoadTexture(TEXTURE_PATH_BRICK);
-	texMario = game->LoadTexture(TEXTURE_PATH_MARIO);
-	texMisc = game->LoadTexture(TEXTURE_PATH_MISC);
-
-	// Load a sprite sheet as a texture to try drawing a portion of a texture. See function Render 
-	//texMisc = game->LoadTexture(MISC_TEXTURE_PATH);
-
-	mario = new CMario(MARIO_START_X, MARIO_START_Y, MARIO_START_VX, MARIO_START_VY, texMario);
-	brick = new CBrick(BRICK_X, BRICK_Y, texBrick);
-
-
-	// objects.push_back(mario);
-	// for(i)		 
-	//		objects.push_back(new CGameObject(BRICK_X+i*BRICK_WIDTH,....);
-	//
-
-	//
-	// int x = BRICK_X;
-	// for(i)
-	//		... new CGameObject(x,.... 
-	//		x+=BRICK_WIDTH;
-}
-
-/*
-	Update world status for this frame
-	dt: time period between beginning of last frame and beginning of this frame
-*/
-void Update(DWORD dt)
-{
-	/*
-	for (int i=0;i<n;i++)
-		objects[i]->Update(dt);
-	*/
-
-	mario->Update(dt);
-	brick->Update(dt);
-
-	//DebugOutTitle(L"01 - Skeleton %0.1f, %0.1f", mario->GetX(), mario->GetY());
-}
-
-/*
-	Render a frame
-*/
-void Render()
-{
-	CGame* g = CGame::GetInstance();
-
-	ID3D10Device* pD3DDevice = g->GetDirect3DDevice();
-	IDXGISwapChain* pSwapChain = g->GetSwapChain();
-	ID3D10RenderTargetView* pRenderTargetView = g->GetRenderTargetView();
-	ID3DX10Sprite* spriteHandler = g->GetSpriteHandler();
-
-	if (pD3DDevice != NULL)
-	{
-		// clear the background 
-		pD3DDevice->ClearRenderTargetView(pRenderTargetView, BACKGROUND_COLOR);
-
-		spriteHandler->Begin(D3DX10_SPRITE_SORT_TEXTURE);
-
-		// Use Alpha blending for transparent sprites
-		FLOAT NewBlendFactor[4] = { 0,0,0,0 };
-		pD3DDevice->OMSetBlendState(g->GetAlphaBlending(), NewBlendFactor, 0xffffffff);
-
-		brick->Render();
-		mario->Render();
-
-		// Uncomment this line to see how to draw a porttion of a texture  
-		//g->Draw(10, 10, texMisc, 300, 117, 317, 134);
-		//g->Draw(10, 10, texMario, 215, 120, 234, 137);
-
-		spriteHandler->End();
-		pSwapChain->Present(0, 0);
-	}
 }
 
 HWND CreateGameWindow(HINSTANCE hInstance, int nCmdShow, int ScreenWidth, int ScreenHeight)
@@ -198,42 +95,6 @@ HWND CreateGameWindow(HINSTANCE hInstance, int nCmdShow, int ScreenWidth, int Sc
 	return hWnd;
 }
 
-int Run()
-{
-	MSG msg;
-	int done = 0;
-	ULONGLONG frameStart = GetTickCount64();
-	ULONGLONG tickPerFrame = 1000 / MAX_FRAME_RATE;
-
-	while (!done)
-	{
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-		{
-			if (msg.message == WM_QUIT) done = 1;
-
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-
-		ULONGLONG now = GetTickCount64();
-
-		// dt: the time between (beginning of last frame) and now
-		// this frame: the frame we are about to render
-		ULONGLONG dt = now - frameStart;
-
-		if (dt >= tickPerFrame)
-		{
-			frameStart = now;
-			Update((DWORD)dt);
-			Render();
-		}
-		else
-			Sleep((DWORD)(tickPerFrame - dt));
-	}
-
-	return 1;
-}
-
 int WINAPI WinMain(
 	_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
@@ -242,16 +103,10 @@ int WINAPI WinMain(
 )
 {
 	HWND hWnd = CreateGameWindow(hInstance, nCmdShow, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-	CGame* game = CGame::GetInstance();
-	game->Init(hWnd);
-
+	
 	SetWindowPos(hWnd, 0, 0, 0, SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, SWP_NOMOVE | SWP_NOOWNERZORDER | SWP_NOZORDER);
 
-
-	LoadResources();
-
-	Run();
-
+	GameLoop* loop = new GameLoop(hWnd);
+	
 	return 0;
 }
